449c8e6678a46f5902f5ef4e484bb4f826b340f4
Olexandr Nikolaevsky <ksander.nik@gmail.com>
2025-03-09 14:54:18 +0000

    my changes

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,22 +4,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = pd.Series( (df['weight'] / (df['height'] / 100) ** 2) > 25, dtype=int)
 
 # 3
-
+df['cholesterol'] = pd.Series(df['cholesterol'] > 1, dtype=int)
+df['gluc'] = pd.Series(df['gluc'] > 1, dtype=int)
+df.head(10)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
 
     # 6
-    df_cat = None
+    df_cat = pd.DataFrame(df_cat.groupby(['cardio', 'variable', 'value'])['value'].count()).rename(columns={'value': 'total'}).reset_index()
     
 
     # 7
@@ -27,7 +29,7 @@ def draw_cat_plot():
 
 
     # 8
-    fig = None
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar').figure
 
 
     # 9
@@ -38,21 +40,27 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    pressure_mask = (df['ap_lo'] <= df['ap_hi'])
+    low_height_mask = df['height'] >= df['height'].quantile(0.025)
+    hi_height_mask = df['height'] <= df['height'].quantile(0.975)
+    low_weight_mask = df['weight'] >= df['weight'].quantile(0.025)
+    hi_weight_mask = df['weight'] <= df['weight'].quantile(0.975)
+    
+    df_heat = df[pressure_mask & low_height_mask & hi_height_mask & low_weight_mask & hi_weight_mask]
 
     # 12
-    corr = None
+    corr = df_heat.corr(numeric_only=True)
 
     # 13
-    mask = None
-
-
-
+    mask = np.zeros_like(corr, dtype=bool)
+    mask[np.triu_indices_from(mask)] = True
+    
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 12))
+
 
     # 15
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f', center=0, square=True, cbar_kws={'shrink': 0.5})
 
 
     # 16
